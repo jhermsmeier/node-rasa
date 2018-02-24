5a9474851dfe7890d3d30b855401660ed44d196e
Jonas Hermsmeier <jhermsmeier@gmail.com>
2018-02-24 16:35:26 +0100

    doc(example): Adjust example to inspect test data

--- example/inspect.js
+++ example/inspect.js
@@ -3,7 +3,7 @@ var inspect = require( '../test/inspect' )
 var path = require( 'path' )
 var argv = process.argv.slice(2)
 
-var filename = argv.shift() || path.join( __dirname, '..', 'test', 'data', 'app.asar' )
+var filename = argv.shift() || path.join( __dirname, '..', 'test', 'data', 'pack.asar' )
 var archive = new rasa.Archive()
 
 function close( error ) {
@@ -15,16 +15,17 @@ archive.open( filename, ( error ) => {
   if( error ) return void close( error )
   inspect.log( archive )
   console.log( '' )
-  // console.log( 'readdir', inspect( archive.readdir( '/browser/api' ) ) )
-  // console.log( 'getFileNode', inspect( archive.getFileNode( 'renderer/api/desktop-capturer.js' ) ) )
+  // console.log( 'readdir', inspect( archive.readdir( '/' ) ) )
+  // console.log( 'getFileNode', inspect( archive.getFileNode( 'archive.js' ) ) )
   console.log( 'files', inspect( rasa.Archive.listFiles( archive.root ) ) )
   console.log( '' )
-  // archive.readFile( 'renderer/api/desktop-capturer.js', ( error, buffer ) => {
+  // archive.readFile( 'archive.js', ( error, buffer ) => {
   //   console.log( error || buffer.toString() )
   //   console.log( '' )
-  //   archive.readStream( 'renderer/api/desktop-capturer.js' )
-  //     .pipe( process.stdout )
-  //     .once( 'end', close )
+  //   // archive.createReadStream( 'archive.js' )
+  //   //   .pipe( process.stdout )
+  //   //   .once( 'end', close )
+  //   close()
   // })
   close()
 })
